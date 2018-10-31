9b2ddb2593788ff923f460ef9e42705c30ec47e4
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2018-10-31 11:14:06 -0400

    modification of the intial generation

--- src/Program.cs
+++ src/Program.cs
@@ -11,16 +11,32 @@ namespace ObstacleTowerGeneration
             for(int i=0; i<100; i++){
                 MissionGraph.Generator mg = new MissionGraph.Generator(new Random());
                 mg.loadPatterns("grammar/");
-                MissionGraph.Graph resultGraph = mg.GenerateDungeon("graphStart.txt", "graphRecipe.txt");
+                MissionGraph.Graph resultGraph = null;
+                for (int j = 0; j < 100; j++){
+                    resultGraph = mg.GenerateDungeon("graphStart.txt", "graphRecipe.txt");
+                    if(resultGraph != null && Helper.checkIsSolvable(resultGraph, resultGraph.nodes[0])){
+                        break;
+                    }
+                    else{
+                        resultGraph = null;
+                    }
+                }
                 if(resultGraph == null){
                     continue;
                 }
                 Console.Write(resultGraph);
-                if(!Helper.checkIsSolvable(resultGraph, resultGraph.nodes[0])){
-                    continue;
-                }
+
                 LayoutGrammar.Generator lg = new LayoutGrammar.Generator(new Random());
-                LayoutGrammar.Map resultMap = lg.generateDungeon(resultGraph);
+                LayoutGrammar.Map resultMap = null;
+                for(int j=0; j<100; j++){
+                    resultMap = lg.generateDungeon(resultGraph);
+                    if(resultMap != null){
+                        break;
+                    }
+                    else{
+                        resultMap = null;
+                    }
+                }
                 if(resultMap == null){
                     continue;
                 }
